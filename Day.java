b59bc08af56444fdb77d01999d33a945132f92af
huttonAddy <hutton.amisonaddy2@gmail.com>
2023-11-27 23:26:36 +0000

    Event Overhaul

--- Day.java
+++ Day.java
@@ -1,34 +1,62 @@
 import java.util.Hashtable;
-import java.sql.Time;
 import java.time.LocalTime;
+import java.util.PriorityQueue;
 
 public class Day {
     private int dayNum;
     private Hashtable<LocalTime, Event> events = new Hashtable<>();
+    private PriorityQueue<LocalTime> startTimes = new PriorityQueue<>();
 
-    public void addEvent(Time t, int duration, Event e) {
+    public boolean addEvent(LocalTime time, int duration, Event e) {
         int count = duration / 15;
-        LocalTime time = t.toLocalTime();
+
+        if (getEvent(time) == null) {
+            return false;
+        } else {
+            startTimes.add(time);
+            for (int i = 0; i < count; i++) {
+                events.put(time, e);
+
+                time = time.plusMinutes(15);
+            }
+            return true;
+        }
+    }
+
+    public void removeEvent(LocalTime time, int duration, Event e) {
+        int count = duration / 15;
 
         for (int i = 0; i < count; i++) {
-            events.put(time, e);
+            events.remove(time);
+            startTimes.remove(time);
             time = time.plusMinutes(15);
         }
     }
 
-    public void removeEvent(Time t, int duration, Event e) {
-        int count = duration / 15;
-        LocalTime time = t.toLocalTime();
-
-        for (int i = 0; i < count; i++) {
-            events.remove(time, e);
-            time = time.plusMinutes(15);
-        }
+    public Event getEvent(LocalTime t) {
+        return events.get(t);
     }
 
+    public Event[] getDayEvents() {
+
+        LocalTime[] pq = (LocalTime[]) startTimes.toArray();
+        Event[] e = new Event[pq.length];
+        for (int i = 0; i < pq.length; i++) {
+            e[i] = getEvent(pq[i]);
+        }
+        return e;
+    }
+
+    public int getDate(){
+        return dayNum;
+    }
+
+    public Day(int dayNum_){
+        this.dayNum=dayNum_;
+    }
     public static void main(String[] args) {
         Hashtable<LocalTime, String> events = new Hashtable<>();
-        
+
         events.put(LocalTime.of(10, 0), "Wake up");
         events.put(LocalTime.of(10, 15), "Wake up");
         events.put(LocalTime.of(12, 0), "Go to class");
