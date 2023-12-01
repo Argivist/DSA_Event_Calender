7960ff71a5ab84b1d916d8ccdfcb3f7d9d4a3ee8
huttonAddy <hutton.amisonaddy2@gmail.com>
2023-12-01 16:25:02 +0000

    Day reverting

--- Day.java
+++ Day.java
@@ -1,59 +1,50 @@
-import java.util.Dictionary;
 import java.util.Hashtable;
 import java.time.LocalTime;
 import java.util.PriorityQueue;
 
 public class Day {
     private int dayNum;
-    private Hashtable<Integer, Event> events = new Hashtable<>();
-    private PriorityQueue<LocalTime> timequeue = new PriorityQueue<>();
-    private Dictionary<LocalTime,Integer> id_dict=new Hashtable<>();
+    private Hashtable<LocalTime, Event> events = new Hashtable<>();
+    private PriorityQueue<LocalTime> startTimes = new PriorityQueue<>();
 
-    public boolean addEvent(Event e) {
-        int id=e.getID();
-        LocalTime time=e.getTime();
-        if (getEvent(id) != null) {
+    public boolean addEvent(LocalTime time, int duration, Event e) {
+        int count = duration / 15;
+
+        if (getEvent(time) == null) {
             return false;
         } else {
-            id_dict.put(time,id);            
-                events.put(id, e);
-                timequeue.add(time);
-            //    time = time.plusMinutes(15);
-            
+            startTimes.add(time);
+            for (int i = 0; i < count; i++) {
+                events.put(time, e);
+
+                time = time.plusMinutes(15);
+            }
             return true;
         }
     }
 
-    public void removeEvent(int id) {
-        //int duration=getEvent(time).getDur();
-        //int count = duration / 15;
+    public void removeEvent(LocalTime time, int duration, Event e) {
+        int count = duration / 15;
 
-            Event e=events.get(id);
-            events.remove(id);
-            timequeue.remove(e.getTime());
-            id_dict.remove(e.getTime());
-            //time = time.plusMinutes(15);
-        
+        for (int i = 0; i < count; i++) {
+            events.remove(time);
+            startTimes.remove(time);
+            time = time.plusMinutes(15);
+        }
     }
 
-    public Event getEvent(int i) {
-        return events.get(i);
+    public Event getEvent(LocalTime t) {
+        return events.get(t);
     }
 
     public Event[] getDayEvents() {
-        Object[] pq = timequeue.toArray();
+
+        LocalTime[] pq = (LocalTime[]) startTimes.toArray();
         Event[] e = new Event[pq.length];
         for (int i = 0; i < pq.length; i++) {
-            e[i] = getEvent((int) id_dict.get(pq[i]));
+            e[i] = getEvent(pq[i]);
         }
         return e;
-    }
-    
-    public void printDayEvents(){
-        Event[] e=getDayEvents();
-        for(Event event: e){
-            System.out.println(event);
-        }
     }
 
     public int getDate(){
@@ -63,18 +54,14 @@ public class Day {
     public Day(int dayNum_){
         this.dayNum=dayNum_;
     }
-    public static void main(String[] args) {    
-        Day d = new Day(1);
-        Event e = new Event(12,"12-03", LocalTime.of(12,0), "Walking", "move to a place");
-        System.out.println(d.addEvent(e));
-        System.out.println(d.getEvent(12));
-        Event e2 = new Event(2,"11-09", LocalTime.of(1,0), "Sweeping", "brooming around");
-        System.out.println(d.addEvent(e2));
-        System.out.println(d.getEvent(2));
-        System.out.println(d.getDayEvents());
+    public static void main(String[] args) {
+        Hashtable<LocalTime, String> events = new Hashtable<>();
 
-        d.printDayEvents();
-    
+        events.put(LocalTime.of(10, 0), "Wake up");
+        events.put(LocalTime.of(10, 15), "Wake up");
+        events.put(LocalTime.of(12, 0), "Go to class");
+
+        System.out.println(events.get(LocalTime.of(10, 0)));
     }
 
 }
---
+++ Day_irrelevant.java
@@ -0,0 +1,82 @@
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.time.LocalTime;
+import java.util.PriorityQueue;
+
+public class Day {
+    private int dayNum;
+    private Hashtable<Integer, Event> events = new Hashtable<>();
+    private PriorityQueue<LocalTime> timequeue = new PriorityQueue<>();
+    private Dictionary<LocalTime,Integer> id_dict=new Hashtable<>();
+
+    public boolean addEvent(Event e) {
+        int id=e.getID();
+        LocalTime time=e.getTime();
+        if (getEvent(id) != null) {
+            return false;
+        } else {
+            id_dict.put(time,id);            
+                events.put(id, e);
+                timequeue.add(time);
+            //    time = time.plusMinutes(15);
+            
+            return true;
+        }
+    }
+
+    public void removeEvent(int id) {
+        //int duration=getEvent(time).getDur();
+        //int count = duration / 15;
+
+            Event e=events.get(id);
+            events.remove(id);
+            timequeue.remove(e.getTime());
+            id_dict.remove(e.getTime());
+            //time = time.plusMinutes(15);
+        
+    }
+
+    public Event getEvent(int i) {
+        return events.get(i);
+    }
+
+    public Event[] getDayEvents() {
+        Object[] pq = timequeue.toArray();
+        Event[] e = new Event[pq.length];
+        for (int i = 0; i < pq.length; i++) {
+            e[i] = getEvent((int) id_dict.get(pq[i]));
+        }
+        return e;
+    }
+    
+    public void printDayEvents(){
+        Event[] e=getDayEvents();
+        for(Event event: e){
+            System.out.println(event);
+        }
+    }
+
+    public int getDate(){
+        return dayNum;
+    }
+
+    public Day(int dayNum_){
+        this.dayNum=dayNum_;
+    }
+    public static void main(String[] args) {    
+        Day d = new Day(1);
+        Event e = new Event(12,"12-03", LocalTime.of(12,0), "Walking", "move to a place");
+        System.out.println(d.addEvent(e));
+        System.out.println(d.getEvent(12));
+        Event e2 = new Event(2,"11-09", LocalTime.of(1,0), "Sweeping", "brooming around");
+        System.out.println(d.addEvent(e2));
+        System.out.println(d.getEvent(2));
+        System.out.println(d.getDayEvents());
+
+        d.printDayEvents();
+    
+    }
+
+}
+
+
