e401a682623af67dd55e12ca58270aac93b52961
quagrain <68133626+quagrain@users.noreply.github.com>
2023-12-01 17:54:10 +0000

    Fixed day and event

--- Day.java
+++ Day.java
@@ -7,34 +7,38 @@ public class Day {
     private Hashtable<LocalTime, Event> events = new Hashtable<>();
     private PriorityQueue<LocalTime> startTimes = new PriorityQueue<>();
 
+
     public boolean addEvent(Event e) {
-        
-        LocalTime time=e.getTime();
-        
-        if (getEvent(time) != null) {
-            return false;
-        } else {
-            startTimes.add(time);
-            
-                events.put(time, e);
-            //    time = time.plusMinutes(15);
-            
-            return true;
+        LocalTime newEventStartTime = e.getStartTime();
+        LocalTime newEventEndTime = e.getEndTime();
+
+        if (getEvent(newEventStartTime) != null) {return false;}
+
+        for (LocalTime existingEventStartTime : startTimes) {
+            Event existingEvent = events.get(existingEventStartTime);
+            LocalTime existingEventEndTime = existingEvent.getEndTime();
+
+            if ((newEventStartTime.isBefore(existingEventEndTime) || newEventStartTime.equals(existingEventEndTime))
+                    && (newEventEndTime.isAfter(existingEventStartTime) || newEventEndTime.equals(existingEventStartTime))) {
+                return false;
+            }
         }
+
+        startTimes.add(newEventStartTime);
+        events.put(newEventStartTime, e);
+        return true;
     }
 
     public void removeEvent(LocalTime time) {
-        //int duration=getEvent(time).getDur();
-        //int count = duration / 15;
-
-        
+        if (events.containsKey(time)) {
             events.remove(time);
             startTimes.remove(time);
-            //time = time.plusMinutes(15);
-        
+        } else {
+            System.out.println("No event found at the specified start time.");
+        }
     }
 
-    public Event getEvent(LocalTime t) {
+    public Event getEvents(LocalTime t) {
         return events.get(t);
     }
 
@@ -49,9 +53,15 @@ public class Day {
     
     public void printDayEvents(){
         Event[] e=getDayEvents();
+
+        if (e.length==0){
+            System.out.println("No events today");
+            return;
+        }
         for(Event event: e){
             System.out.println(event);
         }
+
     }
 
     public int getDate(){
@@ -63,15 +73,13 @@ public class Day {
     }
     public static void main(String[] args) {    
         Day d = new Day(1);
-        Event e = new Event(2,"12-03", LocalTime.of(12,0), "Walking", "move to a place");
+        Event e = new Event(2,"12-03", LocalTime.of(12,0), LocalTime.of(13,0), "Walking", "move to a place");
+        Event e2 = new Event(3,"11-09", LocalTime.of(12,10), LocalTime.of(12,20), "Sweeping", "brooming around");
+
         System.out.println(d.addEvent(e));
-        System.out.println(d.getEvent(LocalTime.of(12, 0)));
-        Event e2 = new Event(3,"11-09", LocalTime.of(1,0), "Sweeping", "brooming around");
         System.out.println(d.addEvent(e2));
-        System.out.println(d.getEvent(LocalTime.of(1, 0)));
 
         d.printDayEvents();
-    
     }
 
 }
